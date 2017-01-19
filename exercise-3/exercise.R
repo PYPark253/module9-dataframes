accd30235940dd6bc2e965520ad94bf00cc9eaef
Phillip Park <phillipypark@gmail.com>
2017-01-19 15:20:44 -0800

    Factor is cool

--- exercise-3/exercise.R
+++ exercise-3/exercise.R
@@ -5,30 +5,33 @@
 # The variable USPersonalExpenditure is now accessible to you. Unfortunately,
 # it's not a data frame (it's actually what is called a matrix)
 # Test this using the `is.data.frame()` function
-
+is.data.frame(USPersonalExpenditure)
 
 # Luckily, you can simply pass the USPersonalExpenditure variable as an argument
 # to the `data.frame()` function to convert it a data farm. Do this, storing the
 # result in a new variable
+data <- data.frame(USPersonalExpenditure)
+View(data)
 
 
 # What are the column names of your dataframe?
+colnames(data)
 
 
 # Why are they so strange? Think about whether you could use a number like 1940
 # with dollar notation!
 
 # What are the row names of your dataframe?
-
+rownames(data)
 
 # Create a column "category" that is equal to your rownames
-
+data$category <-rownames(data)
 
 # How much money was spent on personal care in 1940?
-
+personal.care <- data['Personal Care' , "x1940"]
 
 # How much money was spent on Food and Tobacco in 1960?
-
+food.tobacco <- data['Food and Tobacco', "x1960"]
 
 # What was the highest expenditure category in 1960?
 # Hint: use the `max()` function to find the largest, then compare that to the column
